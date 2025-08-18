97029dc89527bdd9763321bb78985728bead6f0a
Shaurya Kumar <kshaurya731@gmail.com>
2025-08-18 19:22:12 -0400

    removed mongodb schema validation cuz it was causing problems

--- src/lib/db/schemas/FormSubmission.ts
+++ src/lib/db/schemas/FormSubmission.ts
@@ -1,20 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
 const formSubmissionSchema = new Schema({
-    id: { type: String, required: true },
-    bias: { type: String, required: true },
-    block: { type: String, required: true },
-    prolificID: { type: String, required: true },
-    demographics: { type: Object, required: true },
-    survey: { type: Object, required: true },
-    choseToHitOptionsSet: { type: String, required: true },
-    preDiscussionChoseToHit: { type: Object, required: true }, 
-    llmConversationMessages: { type: Object, required: true }, 
-    postDiscussionChoseToHit: { type: Object, required: true }, 
-    modelLLMConversationSummary: { type: String, required: true },
-    llmConversationSummarizedBy: { type: String, default: null }, 
-    referenceFormSubmissionID: { type: String, default: null }, 
-    isReferenced: { type: Boolean, default: false }
+    id: { type: String, required: false },
+    bias: { type: String, required: false },
+    block: { type: String, required: false },
+    prolificID: { type: String, required: false },
+    demographics: { type: Object, required: false },
+    survey: { type: Object, required: false },
+    choseToHitOptionsSet: { type: String, required: false },
+    preDiscussionChoseToHit: { type: Object, required: false }, 
+    llmConversationMessages: { type: Object, required: false }, 
+    postDiscussionChoseToHit: { type: Object, required: false }, 
+    modelLLMConversationSummary: { type: String, required: false },
+    llmConversationSummarizedBy: { type: String, default: null, required: false }, 
+    referenceFormSubmissionID: { type: String, default: null, required: false }, 
+    isReferenced: { type: Boolean, default: false, required: false }
 });
 
 const FormSubmission = mongoose.models.formSubmission || mongoose.model('formSubmission', formSubmissionSchema);
